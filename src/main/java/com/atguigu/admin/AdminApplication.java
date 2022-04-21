f450bae281f9cf61213830e427b7a848f5ad61b6
dragon <1032065185@qq.com>
2022-04-21 15:50:11 +0800

    second commit

--- src/main/java/com/atguigu/admin/AdminApplication.java
+++ src/main/java/com/atguigu/admin/AdminApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdminApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AdminApplication.class, args);
+        System.out.println("hello,git");
     }
 
 }
